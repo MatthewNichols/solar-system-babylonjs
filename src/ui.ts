2d4ea6d1729b3a4cef9115cfda8390fde09a51d6
Matthew Nichols <matthew@matthew-nichols.com>
2022-02-13 21:10:39 +0100

    Redid the Zoom UI with a slider.

--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,8 @@
 import { TransformNode } from "babylonjs";
 import * as GUI from "babylonjs-gui";
 
+const zoomMultiplier = 0.5;
+
 export const createZoomButton = (uiLayer: GUI.AdvancedDynamicTexture, buttonName: string, buttonText: string, horizontalAlignment: number, verticalAlignment: number, action: () => void) => {
     const zoomButton = GUI.Button.CreateSimpleButton(buttonName, buttonText);
     zoomButton.height = "60px";
@@ -16,14 +18,24 @@ export const createZoomButton = (uiLayer: GUI.AdvancedDynamicTexture, buttonName
     zoomButton.onPointerUpObservable.add(action);
   }
   
-  export const setupUI = (cameraControl: TransformNode) => {
+  export const setupUI = async (cameraControl: TransformNode) => {
     const uiLayer = GUI.AdvancedDynamicTexture.CreateFullscreenUI("Controls");
-    createZoomButton(uiLayer, "ZoomIn", "+", GUI.Control.HORIZONTAL_ALIGNMENT_LEFT, GUI.Control.VERTICAL_ALIGNMENT_BOTTOM, () => {
-      cameraControl.position.y = cameraControl.position.y - 30;
-    });
+    await uiLayer.parseFromURLAsync("guiTexture.json");
+
+    const zoomSlider = uiLayer.getControlByName("ZoomSlider") as GUI.Slider;
+
+    console.log(cameraControl.position.y);
+    zoomSlider.onValueChangedObservable.add((value: number) => {
+      console.log(value)
+      cameraControl.position.y = value * zoomMultiplier;
+    })
+
+    // createZoomButton(uiLayer, "ZoomIn", "+", GUI.Control.HORIZONTAL_ALIGNMENT_LEFT, GUI.Control.VERTICAL_ALIGNMENT_BOTTOM, () => {
+    //   cameraControl.position.y = cameraControl.position.y - 30;
+    // });
   
-    createZoomButton(uiLayer, "ZoomOut", "-", GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT, GUI.Control.VERTICAL_ALIGNMENT_BOTTOM, () => {
-      cameraControl.position.y = cameraControl.position.y + 30;
-    });
+    // createZoomButton(uiLayer, "ZoomOut", "-", GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT, GUI.Control.VERTICAL_ALIGNMENT_BOTTOM, () => {
+    //   cameraControl.position.y = cameraControl.position.y + 30;
+    // });
   
   }
